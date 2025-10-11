ea5d1e1bf28f7dd75475eeb8f29f8759e0ba7d49
Tor <tor.loejborg@ufst.dk>
2025-10-11 14:27:28 +0200

    update js for temporal

--- middleware/temporal/temporal.ek.js
+++ middleware/temporal/temporal.ek.js
@@ -1,11 +1,11 @@
 let configuration = {
     "description": "An opensource workflow engine",
-    "dependsOn" : [ "postgres", "ingress" ],
-    "extraPorts" : [
+    "dependsOn": ["postgres", "ingress"],
+    "extraPorts": [
         {
-            "hostPort" : 7233,
-            "nodePort" : 30950,
-            "protocol" : "TCP"
+            "hostPort": 7233,
+            "nodePort": 30950,
+            "protocol": "TCP"
         }
     ]
 }
@@ -20,18 +20,25 @@ const images = new Map([
     ["temporalio/ui:2.19.0", "localhost:5001/temporalio/ui:2.19.0"]
 ])
 
-
 easykube
     .preload(images)
     .kustomize()
     .waitForDeployment(deployment, namespace)
 
-easykube.runCommand(deployment, namespace,"tctl",["--ns", "namespace-a", "n" ,"re"])
-    .onSuccess((r)=>{console.info(r)})
-    .onFail((e)=>{console.warn(e)})
+function createTemporalNamespace(name) {
 
+    easykube.runCommand(deployment, namespace, "tctl", ["--ns", name, "n", "re"])
+        .onSuccess((r) => {
+            console.info(r)
+        })
+        .onFail((e) => {
+            if (msg.contains("Namespace already exists")) {
+                console.info("Namespace already registered")
+            } else {
+                console.error(msg)
+            }
+        })
+}
 
-easykube.runCommand(deployment,namespace,"tctl",["--ns", "namespace-b", "n" ,"re"])
-    .onSuccess((r)=>{console.info(r)})
-    .onFail((e)=>{console.warn(e)})
-
+createTemporalNamespace("namespace-a")
+createTemporalNamespace("namespace-b")
